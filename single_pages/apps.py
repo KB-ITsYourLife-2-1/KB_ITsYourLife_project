5eb548c3325e65158d086142dfb83a133265d5e3
YongJun-Bae <dhcmdksk91@gmail.com>
2022-10-26 10:34:53 +0900

    스케쥴러 설정

--- single_pages/apps.py
+++ single_pages/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class SinglePagesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "single_pages"
+
+    def ready(self):
+        from jobs import updater
+        updater.start()
